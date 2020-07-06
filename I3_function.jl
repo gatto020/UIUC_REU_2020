6fd43eb00cea6591419e41013f068f97b3c50df9
gatto020 <67279971+gatto020@users.noreply.github.com>
2020-07-06 14:26:17 -0500

    Add files via upload

---
+++ I3_function.jl
@@ -0,0 +1,29 @@
+using QBase
+
+#Calculates 3-party Multivariate Mutual Information for a given density matrix of a 3-party systemerror
+#I3(A:B:C) = S(ABC)-S(AB)-S(BC)-S(AC)+S(A)+S(B)+S(C)
+#Only works if subsystems share qubits (otherwise partial_trace would not work)
+function I3(density_matrix,n) #n is the total number of qubits
+    if (n == 3)
+        marginal_AB = QBase.partial_trace(density_matrix,[2,2,2],3)
+        marginal_BC = QBase.partial_trace(density_matrix,[2,2,2],1)
+        marginal_AC = QBase.partial_trace(density_matrix,[2,2,2],2)
+        marginal_A = QBase.partial_trace(marginal_AB,[2,2],2)
+        marginal_B = QBase.partial_trace(marginal_BC,[2,2],2)
+        marginal_C = QBase.partial_trace(marginal_AC,[2,2],1)
+        i3 = QBase.Information.von_neumann_entropy(density_matrix) - QBase.Information.von_neumann_entropy(marginal_AB) - QBase.Information.von_neumann_entropy(marginal_BC)-QBase.Information.von_neumann_entropy(marginal_AC) + QBase.Information.von_neumann_entropy(marginal_A)+QBase.Information.von_neumann_entropy(marginal_B)+QBase.Information.von_neumann_entropy(marginal_C)
+    elseif(n == 6)
+        marginal_AABB = QBase.partial_trace(QBase.partial_trace(density_matrix,[2,2,2,2,2,2],6),[2,2,2,2,2],5)
+        marginal_BBCC = QBase.partial_trace(QBase.partial_trace(density_matrix,[2,2,2,2,2,2],1),[2,2,2,2,2],1)
+        marginal_AACC = QBase.partial_trace(QBase.partial_trace(density_matrix,[2,2,2,2,2,2],3),[2,2,2,2,2],3)
+        marginal_AA = QBase.partial_trace(QBase.partial_trace(marginal_AABB,[2,2,2,2],4),[2,2,2],3)
+        marginal_BB = QBase.partial_trace(QBase.partial_trace(marginal_BBCC,[2,2,2,2],4),[2,2,2],3)
+        marginal_CC = QBase.partial_trace(QBase.partial_trace(marginal_AACC,[2,2,2,2],1),[2,2,2],1)
+        i3 = QBase.Information.von_neumann_entropy(density_matrix) - QBase.Information.von_neumann_entropy(marginal_AABB) - QBase.Information.von_neumann_entropy(marginal_BBCC)-QBase.Information.von_neumann_entropy(marginal_AACC) + QBase.Information.von_neumann_entropy(marginal_AA)+QBase.Information.von_neumann_entropy(marginal_BB)+QBase.Information.von_neumann_entropy(marginal_CC)
+    else
+        print("Invalid number of qubits")
+    end
+    return i3 
+end
+
+
